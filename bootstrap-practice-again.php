d10b202e955cdab6cd2bc1b19ed4bb0c42a4106a
Jacob Findley <jfindley2@cnm.edu>
2016-01-28 08:18:29 -0700

    Updated bootstrap-practice

--- bootstrap-practice-again.php
+++ bootstrap-practice-again.php
@@ -5,6 +5,28 @@
 		<title>Bootstrap Practice</title>
 	</head>
 	<body>
-
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-6">
+					<img src="">
+				</div>
+				<div class="col-xs-6">
+					<a href="">The Link</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12">
+					<h1>Content Area</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-6">
+					<p>Content area</p>
+				</div>
+				<div class="col-xs-6">
+					<p>Content area 2: Content harder</p>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
